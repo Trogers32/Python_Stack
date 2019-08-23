6ca9a21e2b23774731542561ddd819f1bad2f60a
Tim <timrogers32@gmail.com>
2019-08-23 14:12:03 -0700

    8/23/2019 Midday Push

--- django/django_full_stack/python_belt/apps/belt/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-from django.views.generic.base import RedirectView
-                    
-urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index')
-]
--- django/django_full_stack/python_belt/apps/belt/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from apps.login_registration.models import *
-import datetime
-import bcrypt
-from django.core.urlresolvers import reverse
-
-def home(request):
-    return render(request, "belt/index.html")
